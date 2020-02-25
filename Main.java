1d7930866e97a8a3b474c15d1ffecbc0a5bb2de2
Vladimir <arshavin69ru@gmail.com>
2020-02-26 02:03:05 +0530

    made it more useful

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Git Demo!");
-        System.out.println("line 2 added");
+        byte[] bytes = "apples are cool".getBytes();
+
+        FileOutputStream fout = null;
+        try {
+            fout = new FileOutputStream("C:\\Users\\Vladimir\\Desktop\\testtext.txt");
+            fout.write(bytes);
+
+        } catch (Exception e) {
+
+        } finally {
+
+            try {
+                if (fout != null)
+                    fout.close();
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
